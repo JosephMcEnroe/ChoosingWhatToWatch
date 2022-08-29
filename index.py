92ccbdbb52937fe93b8d6f3d874d4bb6e5f8b9db
JosephMcE890 <jcm1664@rit.edu>
2022-08-29 18:22:06 -0400

    added inwriting to the program

--- index.py
+++ index.py
@@ -7,11 +7,11 @@ tvPath = os.path.join(desktop, "tvShow.txt") #importing data for TV shows
 moviePath = os.path.join(desktop, "movies.txt") #importing data for move
 
 #reading tv show file
-t = open(tvPath,"r") #important lesson - open doesn't mean read**
+t = open(tvPath,"r+") #important lesson - open doesn't mean read**
 tvRead = t.read() 
 
 #reading for Movie file
-m = open(moviePath, "r")
+m = open(moviePath, "r+")
 movieRead = m.read()
 
 #turning files into list
@@ -23,6 +23,8 @@ def menu():
     print("\t\tMenu")
     print("1)\tChoose a Random TV Show")
     print("2)\tChoose a Random Movie")
+    print("3)\tAdd a TV Show to Watch")
+    print("4)\tAdd a Movie to Watch")
 
 def choice():
     #takes in choice and validates it to see if in range
@@ -38,6 +40,30 @@ def randomNum(choice):
         return(randint(0,len(tvList)-1))
     elif choice == 2:
         return(randint(0,len(movieList)-1))
+    else: 
+        return None
+
+def addtoList(choice):
+    #adds to data file
+    if choice == 3:
+        addingList = input("What do you want to add to list(split TV shows using ','):")
+        t.write("," + addingList)
+    if choice == 4:
+        addingList = input("What do you want to add to list(split movies using ','):")
+        m.write(addingList)
+    else:
+        return None
+    
+    #updating program
+    tvRead = t.read() 
+    movieRead = m.read()
+
+    #updating list
+    tvList = list(tvRead.split(","))
+    movieList = list(movieRead.split(","))
+    print(tvList)
+    print(movieList)
+    
 def printChoice(choice,rand):
     #prints out choice for the night
     if choice == 1:
@@ -50,5 +76,6 @@ def printChoice(choice,rand):
 
 pick = choice()
 randomChoice = randomNum(pick)
+addtoList(pick)
 printChoice(pick,randomChoice)
 
